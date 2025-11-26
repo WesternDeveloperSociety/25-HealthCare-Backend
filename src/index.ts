2eeb2125efbf25d1b78451289a6105a6097d6b89
nicholas <nicholas.look@outlook.com>
2025-11-26 18:25:28 -0500

    defined api routes for documents, appointments and users

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import express from "express";
+import usersRouter from "./routes/users.routes.js";
+import appointmentsRouter from "./routes/appointments.routes.js";
+import documentsRouter from "./routes/documents.routes.js";
 
 const app = express();
 const port = 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/users", usersRouter);
+app.use("/api/appointments", appointmentsRouter);
+app.use("/api/documents", documentsRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, TypeScript + Express!");

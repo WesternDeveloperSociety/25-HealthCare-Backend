e22ffd0356be2561324f3a401ccf45c61711c19f
nicholas <nicholas.look@outlook.com>
2025-11-27 20:12:04 -0500

    moved port to .env and removed test endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
+import "dotenv/config";
 import express from "express";
 import usersRouter from "./routes/users.routes.js";
 import appointmentsRouter from "./routes/appointments.routes.js";
 import documentsRouter from "./routes/documents.routes.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 // Middleware
 app.use(express.json());
@@ -13,10 +14,6 @@ app.use(express.json());
 app.use("/api/users", usersRouter);
 app.use("/api/appointments", appointmentsRouter);
 app.use("/api/documents", documentsRouter);
-
-app.get("/", (req, res) => {
-  res.send("Hello, TypeScript + Express!");
-});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
